e006cef4711be1bf964471ab49590a0d5547cd50
Chandan Chaudhary <chandankkumar156@gmail.com>
2026-05-19 09:36:02 +0530

    Add routing and components for employee management app
    - Implement routing in app.routes.ts with paths for login, dashboard, and employee form.
    - Create Dashboard, Department, Designation, EmployeeForm, EmployeeList, Header, and Login components with their respective HTML, CSS, and spec files.
    - Update tsconfig.app.json to set rootDir to 'src'.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Header } from './pages/header/header';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { EmployeeForm } from './pages/employee-form/employee-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: '',
+        component: Header,
+        children: [
+            {
+                path: 'dashboard',
+                component: Dashboard
+            },
+            {
+                path: 'new employee',
+                component: EmployeeForm
+            }
+        ]
+    }
+];
