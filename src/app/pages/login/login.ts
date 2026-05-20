f0bf5879fa0162aab6b82b220b106661f6fca457
Chandan Chaudhary <chandankkumar156@gmail.com>
2026-05-20 18:49:21 +0530

    feat: update login API endpoint and enhance login functionality

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -16,17 +17,21 @@ export class Login {
   };
 
   http = inject(HttpClient);
+  router = inject(Router);
 
   login() {
     this.http.post(
-      'http://localhost:5109/api/EmployeeMaster/login',
+      'http://localhost:7011/api/EmployeeMaster/login',
       this.loginObj
     ).subscribe({
       next: (result: any) => {
         debugger;
+        localStorage.setItem('empLoginUser', JSON.stringify(result.data));
+        this.router.navigateByUrl("dashboard");
       },
       error: (err: any) => {
        debugger;
+       alert(err.error.message);
       }
     });
   }
