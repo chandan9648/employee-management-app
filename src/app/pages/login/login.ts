eb845c0255901aaf2ae5b7bfaeae8aca47e8345c
Chandan Chaudhary <chandankkumar156@gmail.com>
2026-05-20 18:15:51 +0530

    feat: implement login functionality with form binding and submission

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+
+  loginObj: any = {
+    email: '',
+    contactNo: '',
+  };
+
+  http = inject(HttpClient);
+
+  login() {
+    this.http.post(
+      'http://localhost:5109/api/EmployeeMaster/login',
+      this.loginObj
+    ).subscribe({
+      next: (result: any) => {
+        debugger;
+      },
+      error: (err: any) => {
+       debugger;
+      }
+    });
+  }
+}
